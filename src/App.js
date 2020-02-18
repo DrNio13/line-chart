280743e2117ab93bef1a2f08e122809e09958995
george <georgios.perivolarakis@gamelounge.com>
2020-02-18 21:29:06 +0100

    changed to two diff datasets

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import { LineChart } from './components/LineChart';
 function App() {
   // TODO date in milliseconds
   // date is relative value to X axis
-  const fetchData = [
+  const randomFetchedDataA = [
     { date: 60, value: 83.30 },
-    { date: 0, value: 10 },
+    { date: 1, value: 10 },
     { date: 85, value: 65.32 },
-    { date: 100, value: 96 },
+    { date: 97, value: 96 },
     { date: 20, value: 30 }
+  ]
+
+  const randomFetchedDataB = [
+    { date: 30, value: 53.30 },
+    { date: 5, value: 35.65 },
+    { date: 55, value: 65.32 },
+    { date: 92, value: 21 },
+    { date: 26, value: 35 }
   ]
 
   const referenceRanges = [19, 39]
@@ -20,13 +28,13 @@ function App() {
 
       <div className='chartWrapper'>
         <h4 className='title'>Line Chart</h4>
-        <LineChart data={fetchData} />
+        <LineChart data={randomFetchedDataA} />
       </div>
 
 
       <div className='chartWrapper'>
         <h4 className='title'>Line Chart with Target Range</h4>
-        <LineChart data={fetchData} withGraphArea referenceRanges={referenceRanges} />
+        <LineChart data={randomFetchedDataB} withGraphArea referenceRanges={referenceRanges} />
       </div>
     </article>
   );
